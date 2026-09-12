89ec1345c9d98c5d81d08e7df0351c64ce93d988
Vivek Gupta <vg8085248@gmail.com>
2026-09-12 05:38:00 +0000

    add new things

--- Binary_search.java
+++ Binary_search.java
@@ -2,20 +2,29 @@ import java.util.*;
 
 class Binary_search{
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        int key = 4;
-
-        int low = arr[0];
-        int high = arr[arr.length - 1];
-        
-        int mid = low + (high - low)/ 2;
-        if(arr[mid] == key){
-            System.out.println("The position is " + arr[mid])
-        }else if(arr[mid] < key){
-            low = mid + 1;
-        }else{
-            high = mid - 1;
+        System.out.println("Enter the size of array : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Enter the element of array : ");
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
         }
-        
+       System.out.println("Enter the search element : ");
+       int key = sc.nextInt();
+        int low = 0;
+        int high = arr.length - 1;
+
+        while(low < high){
+            int mid = low + (high - low)/ 2;
+            if(arr[mid] == key){
+                System.out.println("The position is " + (mid + 1));
+                return;
+            }else if(arr[mid] < key){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
     }
 }
